dad8b6a39bf6cf477992a2e8dc18c75328500f06
dhruvsharma123 <dhruv1931.be21@chitkara.edu.in>
2025-03-01 16:49:04 +0530

    Dhruv Sharma Add:Harry's Age

---
+++ HarryAge.java
@@ -0,0 +1,8 @@
+class HaaryAge{
+	public static void main(String[] args){
+		int birthYear = 2000;
+		int currentYear = 2024;
+		
+		System.out.print("Harry's age in 2024 is " + (currentYear-birthYear));
+		}
+	}	
