658190672cadc8ba55b50fcb90bbd27b809f88fe
dhruvsharma123 <dhruv1931.be21@chitkara.edu.in>
2025-03-01 16:59:14 +0530

    Dhruv Sharma Add: Average marks

---
+++ SamAverageMarks.java
@@ -0,0 +1,12 @@
+class SamAverageMarks {
+	public static void main(String[] args){
+		int marksInMaths= 94;
+		int marksInPhyscis= 95;
+		int marksInChemistry= 96;
+		
+		int average = (marksInChemistry+marksInMaths+marksInPhyscis)/3;
+		
+		System.out.println("Sam's average mark in PCM is "+average);
+		}
+	}	
+	
